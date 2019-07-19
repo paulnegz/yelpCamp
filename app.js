7bd27368a6411d7ce7f4dbf5d389658f4ee05f46
root <paul.negedu@yahoo.com>
2019-07-19 03:13:50 +0000

    here

--- app.js
+++ app.js
@@ -61,4 +61,6 @@ app.use("/campgrounds",campgroundsRoutes);
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("The YelpCamp Server Has Started!");
 });
-
+// app.listen(3000, function(){
+//    console.log("The YelpCamp Server Has Started!");
+// });
